5d05faa0c02c067cccf5d848ddfd753269d30095
Edwin Xu <edwinhuish@gmail.com>
2020-04-03 16:30:37 +0800

    fix changeHtmlCharsetToUTF8(string $html)

--- src/Dom/Query.php
+++ src/Dom/Query.php
@@ -288,13 +288,13 @@ class Query
 
     protected function changeHtmlCharsetToUTF8(string $html)
     {
-        preg_match('/<meta[^>]+charset=([^"^\'^;^\s]*)[^>]*>/', $html, $matches);
+        preg_match('/<meta[^>]+charset=[\'"]?([^"^\'^;^\s]*)[\'"]?[^>]*>/', $html, $matches);
 
-        $charset = $matches[1] ?? 'auto';
+        $charset = $matches[1] || 'auto';
 
         $newHtml = mb_convert_encoding($html, "UTF-8", $charset);
 
-        return $charset === 'auto' ? $newHtml : preg_replace('/(<meta[^>]+)charset='.$matches[1].'([^>]*>)/', '${1}charset=UTF-8${2}', $newHtml);
+        return $charset === 'auto' ? $newHtml : preg_replace('/(<meta[^>]+charset=[\'"]?)([^"^\'^;^\s]*)([\'"]?[^>]*>)/', '${1}UTF-8${2}', $newHtml);
     }
 
 }
