20703ed1000de73d7b6264b79bfe94280d3c2d05
Edwin Xu <edwinhuish@gmail.com>
2020-04-04 16:44:17 +0800

    update doc

--- src/Contracts/ExtAttributesHandlerContract.php
+++ src/Contracts/ExtAttributesHandlerContract.php
@@ -5,6 +5,10 @@ namespace QL\Contracts;
 interface ExtAttributesHandlerContract
 {
     /**
+     * This function just work for the first one in case of the return type can be any!!
+     *
+     * 由于返回类型可以是任意类型，此函数仅会执行第一个加入的 handler ！！
+     *
      * @param  array  $attr_array
      * @param  array|object  $rule
      * @param  string|null  $range
